daa81aa7a7acaff276c0731803277729c033cbeb
GITIBB <Tobrode@gmail.com>
2025-09-06 23:07:33 +0200

    added main url argument logic

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+
+	urlArg := os.Args[1:]
+	if len(urlArg) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(urlArg) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	if len(urlArg) == 1 {
+		fmt.Printf("starting crawl of %s\n", urlArg[0])
+	}
+
+	fmt.Println("Hello, World!")
 }
